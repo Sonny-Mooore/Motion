0ac914ab4a8c46d8a24216ce9c040de2c620b041
Sonny-Mooore <110048965+Sonny-Mooore@users.noreply.github.com>
2026-03-01 02:58:00 +0300

    burger

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,11 +10,8 @@ export default function Home() {
 
   const [isContentOpen, setIsContentOpen] = useState(false);
 
-
-  // ✅ закрываем именно Content
   const handleContentPopupClose = () => setIsContentOpen(false);
 
-  // ✅ логика "первый раз — показать Content, дальше — редирект"
   const handleConnectClick = () => {
     if (typeof window === 'undefined') return;
 
@@ -29,55 +26,61 @@ export default function Home() {
     window.location.href = 'https://t.me/motionvpnbot';
   };
 
-
   const handleClosePopup = () => setOpen(!open);
 
-  return (<div className="page">
-    <img src="/images/noize.png" className="noise" />
-    <div className={'blur-1'} />
-    <div className={'blur-2'} />
-    <main className="content">
-      <Header handleClosePopup={handleClosePopup} />
-      <img
-        className="background"
-        src="/images/background.svg"
-        alt="background"
-      />
-      <div className={'title_container'}>
-        <h1 className={'title'}>Motion — включай мир на полную</h1>
-        <div className={'subtitle'}>
-          Быстрые сервера в актуальных точках мира, стабильное подключение и
-          бесплатный недельный период в два клика
-        </div>
-        <div className="glass_button_container">
-          <button
-            onClick={handleConnectClick}
-            className="glass_button"
-            onMouseMove={(e) => {
-              const rect = e.currentTarget.getBoundingClientRect();
-              const x = e.clientX - rect.left;
-              const y = e.clientY - rect.top;
+  return (
+    <div className="page">
+      <img src="/images/noize.png" className="noise" />
+      <div className={'blur-1'} />
+      <div className={'blur-2'} />
+      <main className="content">
+        <Header handleClosePopup={handleClosePopup} />
+        <img
+          className="background"
+          src="/images/background.svg"
+          alt="background"
+        />
+        <img
+          className="background_mobile"
+          src="/images/background_mobile.svg"
+          alt="background_mobile"
+        />
+        <div className={'title_container'}>
+          <h1 className={'title'}>Motion — включай мир на полную</h1>
+          <div className={'subtitle'}>
+            Быстрые сервера в актуальных точках мира, стабильное подключение и
+            бесплатный недельный период в два клика
+          </div>
+          <div className="glass_button_container">
+            <button
+              onClick={handleConnectClick}
+              className="glass_button"
+              onMouseMove={(e) => {
+                const rect = e.currentTarget.getBoundingClientRect();
+                const x = e.clientX - rect.left;
+                const y = e.clientY - rect.top;
 
-              e.currentTarget.style.setProperty('--x', `${x}px`);
-              e.currentTarget.style.setProperty('--y', `${y}px`);
-            }}
-          >
-            Подключить
-            <img alt={''} src={'/images/Vector.svg'} />
-          </button>
-          <img
-            width={109}
-            height={19}
-            src={'/images/systems.svg'}
-            className="glass_button_icons"
-          />
+                e.currentTarget.style.setProperty('--x', `${x}px`);
+                e.currentTarget.style.setProperty('--y', `${y}px`);
+              }}
+            >
+              Подключить
+              <img alt={''} src={'/images/Vector.svg'} />
+            </button>
+            <img
+              width={109}
+              height={19}
+              src={'/images/systems.svg'}
+              className="glass_button_icons"
+            />
+          </div>
         </div>
-      </div>
-    </main>
-    <Device state={open} handleClosePopup={handleClosePopup} />
-    <Content
-      isContentOpen={isContentOpen}
-      handleContentPopupClose={handleContentPopupClose}
-    />
-  </div>);
+      </main>
+      <Device state={open} handleClosePopup={handleClosePopup} />
+      <Content
+        isContentOpen={isContentOpen}
+        handleContentPopupClose={handleContentPopupClose}
+      />
+    </div>
+  );
 }
