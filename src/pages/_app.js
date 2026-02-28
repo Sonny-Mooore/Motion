9c9f668597fad8aed1189e783158b02e5a37ad0d
Sonny-Mooore <110048965+Sonny-Mooore@users.noreply.github.com>
2026-03-01 00:52:44 +0300

    метрика

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,56 @@
-import '@/styles/globals.css';
+import { useEffect } from 'react';
+import Router from 'next/router';
+import Script from 'next/script';
+
+const YM_ID = Number(process.env.NEXT_PUBLIC_YM_ID || '107053828');
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      if (typeof window !== 'undefined' && typeof window.ym === 'function') {
+        window.ym(YM_ID, 'hit', url);
+      }
+    };
+
+    Router.events.on('routeChangeComplete', handleRouteChange);
+    return () => Router.events.off('routeChangeComplete', handleRouteChange);
+  }, []);
+
+  return (
+    <>
+      <Script
+        id="yandex-metrica"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+            ym(${YM_ID}, "init", {
+              defer: true,
+              clickmap:true,
+              trackLinks:true,
+              accurateTrackBounce:true,
+              webvisor:true
+            });
+          `,
+        }}
+      />
+
+      <noscript>
+        <div>
+          <img
+            src={`https://mc.yandex.ru/watch/${YM_ID}`}
+            style={{ position: 'absolute', left: '-9999px' }}
+            alt=""
+          />
+        </div>
+      </noscript>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
