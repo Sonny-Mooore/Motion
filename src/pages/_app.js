698c884e23ec37c7432d1ef783b863eb683f0044
sonny <sinfricmladshi@gmail.com>
2026-03-02 02:55:18 +0300

    metrica

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,55 +1,56 @@
-import { useEffect } from 'react';
-import Router from 'next/router';
-import Script from 'next/script';
-import '@/styles/globals.css';
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import Script from "next/script";
+import "@/styles/globals.css";
 
-
+const YM_ID = 107053828; // можешь заменить на process.env.NEXT_PUBLIC_YM_ID
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+
   useEffect(() => {
     const handleRouteChange = (url) => {
-      if (typeof window !== 'undefined' && typeof window.ym === 'function') {
-        window.ym(YM_ID, 'hit', url);
+      if (typeof window !== "undefined" && typeof window.ym === "function") {
+        window.ym(YM_ID, "hit", url);
       }
     };
 
-    Router.events.on('routeChangeComplete', handleRouteChange);
-    return () => Router.events.off('routeChangeComplete', handleRouteChange);
-  }, []);
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
 
   return (
     <>
       <Script
-        id="yandex-metrica"
+        id="yandex-metrika"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
-            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-            m[i].l=1*new Date();
-            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+            (function(m,e,t,r,i,k,a){
+              m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {
+                if (document.scripts[j].src === r) { return; }
+              }
+              k=e.createElement(t),
+              a=e.getElementsByTagName(t)[0],
+              k.async=1,
+              k.src=r,
+              a.parentNode.insertBefore(k,a)
+            })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-            ym(107053828, "init", {
-              defer: true,
-              clickmap:true,
-              trackLinks:true,
-              accurateTrackBounce:true,
-              webvisor:true
+            ym(${YM_ID}, "init", {
+              clickmap: true,
+              trackLinks: true,
+              accurateTrackBounce: true,
+              webvisor: true,
+              ecommerce: "dataLayer"
             });
           `,
         }}
       />
-
-      <noscript>
-        <div>
-          <img
-            src={`https://mc.yandex.ru/watch/${YM_ID}`}
-            style={{ position: 'absolute', left: '-9999px' }}
-            alt=""
-          />
-        </div>
-      </noscript>
 
       <Component {...pageProps} />
     </>
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,14 +1,25 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import { Html, Head, Main, NextScript } from "next/document";
+
+const YM_ID = 107053828;
 
 export default function Document() {
   return (
     <Html lang="ru">
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5" />
-      </Head>
+      <Head />
       <body>
-        <Main />
-        <NextScript />
+      <Main />
+      <NextScript />
+
+      <noscript>
+        <div>
+          <img
+            src={`https://mc.yandex.ru/watch/${YM_ID}`}
+            style={{ position: "absolute", left: "-9999px" }}
+            alt=""
+          />
+        </div>
+      </noscript>
+
       </body>
     </Html>
   );
