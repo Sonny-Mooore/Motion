cd032b56733d57116ff889070d602e460d556bd0
Sonny-Mooore <110048965+Sonny-Mooore@users.noreply.github.com>
2026-03-09 14:57:43 +0300

    1 commit

--- src/componets/Footer/Footer.jsx
+++ src/componets/Footer/Footer.jsx
@@ -2,79 +2,80 @@ import React from 'react';
 import { router } from 'next/client';
 
 const Footer = () => {
-  return (<div className={'footer'}>
-    <div className={'footer_column'}>
-      <img src="/images/footer/footer_logo.svg" alt="" />
-      <img src="/images/footer/Copyright.svg" alt="" />
-    </div>
-    <div className={'row_footer'}>
+  return (
+    <div className={'footer'}>
       <div className={'footer_column'}>
-        <div className={'footer_title'}>
-          Инструкция
-        </div>
-        <div onClick={() => router.push('https://t.me/motionhelper')} className={'footer_nav'}>
-          На телефон
-        </div>
-        <div onClick={() => router.push('https://t.me/MotionSize/39')} className={'footer_nav'}>
-          На компьютер
-        </div>
+        <img className={"footer_logo"} src="/images/footer/footer_logo.svg" alt="" />
+        <img className={"Copyright"} src="/images/footer/Copyright.svg" alt="" />
       </div>
-      <div className={'footer_column'}>
-        <div className={'footer_title'}>
-          Навигация
+      <div className={'row_footer'}>
+        <div className={'footer_column'}>
+          <div className={'footer_title'}>
+            Инструкция
+          </div>
+          <div onClick={() => router.push('https://t.me/motionhelper')} className={'footer_nav'}>
+            На телефон
+          </div>
+          <div onClick={() => router.push('https://t.me/MotionSize/39')} className={'footer_nav'}>
+            На компьютер
+          </div>
         </div>
-        <div className={'footer_nav'}
-             onClick={() => {
-               document.getElementById('nav')?.scrollIntoView({
-                 behavior: 'smooth',
-               });
-             }}>
-          Главная
+        <div className={'footer_column'}>
+          <div className={'footer_title'}>
+            Навигация
+          </div>
+          <div className={'footer_nav'}
+               onClick={() => {
+                 document.getElementById('nav')?.scrollIntoView({
+                   behavior: 'smooth',
+                 });
+               }}>
+            Главная
+          </div>
+          <div onClick={() => {
+            document.getElementById('tariffs')?.scrollIntoView({
+              behavior: 'smooth',
+            });
+          }} className={'footer_nav'}>
+            Тарифы
+          </div>
+          <div onClick={() => {
+            document.getElementById('devices')?.scrollIntoView({
+              behavior: 'smooth',
+            });
+          }} className={'footer_nav'}>
+            Устройства
+          </div>
+          <div onClick={() => {
+            document.getElementById('faq')?.scrollIntoView({
+              behavior: 'smooth',
+            });
+          }} className={'footer_nav'}>
+            FAQ
+          </div>
         </div>
-        <div onClick={() => {
-          document.getElementById('tariffs')?.scrollIntoView({
-            behavior: 'smooth',
-          });
-        }} className={'footer_nav'}>
-          Тарифы
+        <div className={'footer_column'}>
+          <div className={'footer_title'}>
+            О проекте
+          </div>
+          <div onClick={() => router.push('/privacy')} className={'footer_nav'}>
+            Политика конфиденциальности
+          </div>
+          <div onClick={() => router.push('/terms')} className={'footer_nav'}>
+            Условия использования
+          </div>
         </div>
-        <div onClick={() => {
-          document.getElementById('devices')?.scrollIntoView({
-            behavior: 'smooth',
-          });
-        }} className={'footer_nav'}>
-          Устройства
-        </div>
-        <div onClick={() => {
-          document.getElementById('faq')?.scrollIntoView({
-            behavior: 'smooth',
-          });
-        }} className={'footer_nav'}>
-          FAQ
-        </div>
-      </div>
-      <div className={'footer_column'}>
-        <div className={'footer_title'}>
-          О проекте
-        </div>
-        <div onClick={() => router.push('/privacy')} className={'footer_nav'}>
-          Политика конфиденциальности
-        </div>
-        <div onClick={() => router.push('/terms')} className={'footer_nav'}>
-          Условия использования
-        </div>
-      </div>
-      <div className={'footer_column'}>
-        <div className={'footer_title'}>
-          Поддержка
-        </div>
-        <div onClick={() => router.push('https://t.me/motionhelper')} className={'footer_nav'}>
-          <img src="/images/faq/tg.svg" alt="" width={18} height={18} />
-          Telegram
-        </div>
+        <div className={'footer_column'}>
+          <div className={'footer_title'}>
+            Поддержка
+          </div>
+          <div onClick={() => router.push('https://t.me/motionhelper')} className={'footer_nav'}>
+            <img src="/images/faq/tg.svg" alt="" width={18} height={18} />
+            Telegram
+          </div>
 
+        </div>
       </div>
-    </div>
-  </div>);
+    </div>);
 };
 export default Footer;
